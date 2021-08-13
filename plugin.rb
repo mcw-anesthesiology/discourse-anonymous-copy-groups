a212eaf60a2f1d7490d3e796c131d325eec0eca0
Jacob Mischka <jacob@mischka.me>
2021-08-13 17:31:19 -0500

    Add after_initialize hook
    Whoops.

--- plugin.rb
+++ plugin.rb
@@ -1,24 +1,27 @@
 # name: anonymous-copy-groups
 # about: A basic plugin to naively copy a user's groups to their anonymous users
-# version: 0.0.3
+# version: 0.1.0
 # authors: Jacob Mischka <jmischka@mcw.edu>
 
 enabled_site_setting :anonymous_copy_groups_enabled
 
-module AnonymousCopyGroupsShadowCreator
-	private def create_shadow!
-		shadow = super
+after_initialize do
+	module AnonymousCopyGroupsShadowCreator
+		private def create_shadow!
+			shadow = super
 
-		user.groups.each do |group|
-			group.add(shadow)
+			user.groups.each do |group|
+				group.add(shadow)
+			end
+
+			shadow.reload
+
+			shadow
 		end
+	end
 
-		shadow.reload
-
-		shadow
+	class ::AnonymousShadowCreator
+		prepend AnonymousCopyGroupsShadowCreator
 	end
 end
 
-class ::AnonymousShadowCreator
-	prepend AnonymousCopyGroupsShadowCreator
-end
