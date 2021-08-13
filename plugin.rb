43496b596e03f53c1774a1604d81b829cfa45d36
Jacob Mischka <jacob@mischka.me>
2021-08-13 16:24:51 -0500

    Use .each instead of for loop

--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,15 @@
 # name: anonymous-copy-groups
 # about: A basic plugin to naively copy a user's groups to their anonymous users
-# version: 0.0.2
+# version: 0.0.3
 # authors: Jacob Mischka <jmischka@mcw.edu>
 
 enabled_site_setting :anonymous_copy_groups_enabled
 
 module AnonymousCopyGroupsShadowCreator
-	private
-
-	def create_shadow!
+	private def create_shadow!
 		shadow = super
 
-		for group in user.groups do
+		user.groups.each do |group|
 			group.add(shadow)
 		end
 
