32d9b5abd243db2bac57ce9ca0d37be9af0ca2f9
Jacob Mischka <jacob@mischka.me>
2021-08-13 14:36:00 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,22 @@
+# name: anonymous-copy-groups
+# about: A basic plugin to naively copy a user's groups to their anonymous users
+# version: 0.0.1
+# authors: Jacob Mischka <jmischka@mcw.edu>
+
+enabled_site_setting :anonymous_copy_groups_enabled
+
+module AnonymousCopyGroupsShadowCreator
+	def create_shadow!
+		shadow = super
+
+		for group in user.groups do
+			group.add(shadow)
+		end
+
+		shadow
+	end
+end
+
+class ::AnonymousShadowCreator
+	prepend AnonymousCopyGroupsShadowCreator
+end
