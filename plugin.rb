34463637d2ace09cf0f83196d382c1174e23058a
Jacob Mischka <jacob@mischka.me>
2021-08-16 05:48:30 -0500

    Don't do anything if plugin isn't enabled

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: anonymous-copy-groups
 # about: A basic plugin to naively copy a user's groups to their anonymous users
-# version: 0.1.0
+# version: 0.1.1
 # authors: Jacob Mischka <jmischka@mcw.edu>
 
 enabled_site_setting :anonymous_copy_groups_enabled
@@ -9,6 +9,8 @@ after_initialize do
 	module AnonymousCopyGroupsShadowCreator
 		private def create_shadow!
 			shadow = super
+
+			return shadow unless :anonymous_copy_groups_enabled
 
 			user.groups.each do |group|
 				group.add(shadow)
